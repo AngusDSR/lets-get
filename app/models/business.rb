0903c45c1377fe612a9bc38a728f3f81c148b003
ilkay <ilkay997@gmail.com>
2022-11-17 21:38:51 +0000

    added businnes model added schema column latitude logitude

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,4 @@
 class Business < ApplicationRecord
+  geocoded_by :street_address
+  after_validation :geocode, if: :will_save_change_to_street_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_190325) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_210641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_190325) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
