ff36f6bdd0757ada486df1da257ff589347e3bc7
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2022-11-17 21:44:23 +0000

    enter-location

--- app/javascript/controllers/enter_locations_controller.js
+++ app/javascript/controllers/enter_locations_controller.js
@@ -4,8 +4,24 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["location1"]
   connect() {
-    // console.log("IT's alive")
   }
+    // console.log("IT's alive")
+    //   const myLatLng = { lat: -25.363, lng: 131.044 };
+    //   const map = new google.maps.Map(this.sourceTarget, {
+    //     zoom: 4,
+    //     center: myLatLng,
+    //   });
+
+    //   new google.maps.Marker({
+    //     position: myLatLng,
+    //     map,
+    //     title: "Hello World!",
+    //   });
+    // }
+
+    // window.initMap = initMap;
+
+
 
   getUserLoc(e) {
     navigator.geolocation.getCurrentPosition((data) => {
