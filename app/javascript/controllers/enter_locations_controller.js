7163d74e186bd019efa542ec0b36556024ee9996
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-11-17 14:07:42 +0000

    stimulus controller generated for entering locations

--- app/javascript/controllers/enter_locations_controller.js
+++ app/javascript/controllers/enter_locations_controller.js
@@ -2,6 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="enter-locations"
 export default class extends Controller {
+  static targets = ["location1"]
   connect() {
+    console.log("IT's alive")
+    console.log(this.location1Target)
+    // console.log(this.itemsTarget)
+    // console.log(this.formTarget)
+  }
+
+  getUserLoc(e) {
+    console.log(e);
+    console.log("clicked");
+    // this.location1Target
   }
 }
