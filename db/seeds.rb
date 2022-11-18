0d9ff234014d4bc3a8ec07abcd069b72f8f6bb5b
safak mengu <mengusafak@gmail.com>
2022-11-18 14:52:46 +0000

    seed updates pubs added

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,33 @@ puts "Dummy user created"
 
 Business.create(
   name:"Assembly House",
-  description: "Greene King company`s lovely pub",
+  description: "Greene King dog friendly pub",
   category: "Pub",
-  street_address: "292-294 Kentish Town Road",
+  street_address: "292-294 Kentish Town Road, London",
   image_url: "Assemblyhouse.jpg"
 )
 puts "Assembly house created"
+Business.create(
+  name:"Royal George",
+  description: "Greene King company`s pub perfect for families",
+  category: "Pub",
+  street_address: "8-14 Eversholt Street, Euston, Greater London",
+  image_url: "theroyalgeorge.jpg"
+)
+puts "Royal George created"
+Business.create(
+  name:"Fitzrovia",
+  description: "Greene King company`s pub perfect for sport",
+  category: "Pub",
+  street_address: "18 Goodge Street, Bloomsbury, Greater London",
+  image_url: "TheFitzrovia.jpg"
+)
+puts "Fitzrovia created"
+Business.create(
+  name:"TRAVELLERS TAVERN",
+  description: "Greene King company`s waterside pub",
+  category: "Pub",
+  street_address: "4 Elizabeth Street, Belgravia, London",
+  image_url: "travellerstavern.jpg"
+)
+puts "TRAVELLERS TAVERN created"
