3e94d5e2559f56b542a36dd208c0dbf3230635c8
ilkay <ilkay997@gmail.com>
2022-11-19 17:50:38 +0000

    business show page

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,5 +1,5 @@
 class BusinessesController < ApplicationController
-  before_action :set_business, only: %i[show]
+  # before_action :set_business, only: %i[show]
   respond_to :html
 
   def index
@@ -12,14 +12,29 @@ class BusinessesController < ApplicationController
   end
 
   def show
+    @business = Business.find(params[:id])
 
+  end
+
+  def create
+    # @business = Business.new(business_params)
+    # @business.user = current_user
+    # if @business.save
+    #   redirect_to businesses_path
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+
+  def new
+    @business = Business.new
   end
 
   private
 
-  def set_business
-    @business = Business.find(params[:id])
-  end
+  # def set_business
+  #   @business = Business.find(params[:id])
+  # end
 
   def business_params
     params.require(:business).permit(:name, :description, :category, :street_address, :image_url)
