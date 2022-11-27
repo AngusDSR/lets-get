2b53d9b0b266d37eb7083b252ac65fd4c72a4a4e
Angus Spawton-Rice <adspawtonrice@gmail.com>
2022-11-27 17:35:03 +0000

    business show

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,8 +1,8 @@
 class Business < ApplicationRecord
-  MAX_DIMS = 100
   TYPES = %w[bar cafe park casino gym spa restaurant museum night_club movie_theater bowling_alley amusement_park art_gallery]
   PHOTO_ROUTE = "https://maps.googleapis.com/maps/api/place/photo?photoreference="
-  PHOTO_PARAMS = "&sensor=false&maxheight=#{MAX_DIMS}&maxwidth=#{MAX_DIMS}&key=#{ENV.fetch('GOOGLE_MAPS_API')}"
+  INDEX_PARAMS = "&sensor=false&maxheight=100&maxwidth=100&key=#{ENV.fetch('GOOGLE_MAPS_API')}"
+  SHOW_PARAMS = "&sensor=false&maxheight=300&maxwidth=300&key=#{ENV.fetch('GOOGLE_MAPS_API')}"
   geocoded_by :street_address
   after_validation :geocode, if: :will_save_change_to_street_address?
   has_many :meets
