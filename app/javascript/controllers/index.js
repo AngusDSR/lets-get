4bdc8e492d85606684d61c0c722f56f96226637b
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2022-11-29 21:30:33 +0000

    typed JS implemented and modifications to buttons on meets businesses page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("enter-locations", EnterLocationsController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
