9ce347b03d923cd26bc0d854781644a2987f662f
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2022-11-30 22:13:42 +0000

    improved layout and animation of homepage typed.js text

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -6,9 +6,16 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Hey there!", "Let's find some suggestions!"],
-      typeSpeed: 25,
-      loop: true
+      strings: ["Hey there!", "Let's find some suggestions!", "Bars, Coffee Shops...", "And many more available!",
+      "Enter your locations above!"],
+      typeSpeed: 15,
+      loop: false,
+      backSpeed: 100,
+      backDelay: 100,
+      fadeOut: true,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 1000,
+      showCursor: true,
     })
   }
 }
